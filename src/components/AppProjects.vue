d2508a9bf6c262534880bd3ddd5480e10334b370
AlessioConsolo <alessioconsolo3@gmail.com>
2024-10-14 22:17:27 +0200

    [UPD] Created AppProjects component and retrieved api with axios

---
+++ src/components/AppProjects.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div></div>
+</template>
+<script>
+import axios from "axios";
+
+export default {
+  name: "AppProjects",
+  data() {
+    return {
+      projects: [],
+    };
+  },
+  created() {
+    this.getProjects();
+  },
+  methods: {
+    getProjects() {
+      axios.get("http://127.0.0.1:8000/api/home").then((results) => {
+        console.log(results);
+      });
+    },
+  },
+};
+</script>
+<style lang=""></style>
